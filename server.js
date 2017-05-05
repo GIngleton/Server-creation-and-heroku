ab4a8e0b3aa727d812964f73a658d39b0273b9c1
GIngleton <georgeingleton@gmail.com>
2017-05-04 20:46:32 -0400

    make it better

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-//environmental variable setup
+//environmental variable setup without storing in system
+
 
 
 require("dotenv").config();
@@ -8,10 +9,17 @@ var app = express();
 // always need the above lines when using express
 // calling for library and then giving express functionality to an express variable
 app.get('/', function(request, respond){
+  if (process.env.NODE_ENV == 'dev'){
   respond.json({
     "user": process.env.DB_USER,
     "pass": process.env.DB_PASS
   });
+} else {
+    respond.json({
+      "user": "Does not exist",
+      "pass": "User does not exist"
+    });
+}
 });
 
 app.listen(8080);
